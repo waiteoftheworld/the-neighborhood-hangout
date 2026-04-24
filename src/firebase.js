a2e6663ef28e2d75325611cbf7690a3deec4ef77
waiteoftheworld <emilee@waiteoftheworld.com>
2026-04-23 19:55:51 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWbtzXb2AlaS-Wjz7ytpZ8QrSkMm3NAl0",
+  authDomain: "the-neighborhood-hangout.firebaseapp.com",
+  projectId: "the-neighborhood-hangout",
+  storageBucket: "the-neighborhood-hangout.firebasestorage.app",
+  messagingSenderId: "937839699278",
+  appId: "1:937839699278:web:a9226b75f2b3983fc32a1f"
+}
+
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
