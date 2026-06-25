ba1b65c6cb46d9234f4dcd19f1b0443c0d7d0088
waiteoftheworld <emilee@waiteoftheworld.com>
2026-06-25 17:19:30 -0500

    Add Firebase Analytics SDK initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWbtzXb2AlaS-Wjz7ytpZ8QrSkMm3NAl0",
@@ -8,9 +9,11 @@ const firebaseConfig = {
   projectId: "the-neighborhood-hangout",
   storageBucket: "the-neighborhood-hangout.firebasestorage.app",
   messagingSenderId: "937839699278",
-  appId: "1:937839699278:web:a9226b75f2b3983fc32a1f"
+  appId: "1:937839699278:web:a9226b75f2b3983fc32a1f",
+  measurementId: "G-RJJFTCZXKN"
 }
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const analytics = getAnalytics(app)
