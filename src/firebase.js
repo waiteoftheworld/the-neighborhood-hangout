1a63441b02b7666f929f61f9d3622fdef5bf0280
waiteoftheworld <emilee@waiteoftheworld.com>
2026-06-25 17:48:32 -0500

    Add Firebase Messaging SDK initialization

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getAnalytics } from 'firebase/analytics'
+import { getMessaging } from 'firebase/messaging'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWbtzXb2AlaS-Wjz7ytpZ8QrSkMm3NAl0",
@@ -17,3 +18,4 @@ export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const analytics = getAnalytics(app)
+export const messaging = getMessaging(app)
