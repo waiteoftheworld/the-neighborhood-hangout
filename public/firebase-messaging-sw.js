8c148a8dccb92389efbf90eb93291fb0d490cbe1
waiteoftheworld <emilee@waiteoftheworld.com>
2026-06-26 17:49:12 -0500

    Fix Firebase SDK version mismatch in messaging service worker (11.0.2 -> 10.12.0)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js')
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js')
 
 firebase.initializeApp({
 apiKey: "AIzaSyDWbtzXb2AlaS-Wjz7ytpZ8QrSkMm3NAl0",
